67e149dea76a97827ec145dd35363689be86a37f
Simpson <k_simpson22@fanshawec.ca>
2017-09-22 10:11:22 -0400

    Updated Javascript File

--- script/main.js
+++ script/main.js
@@ -1,1 +1,17 @@
+(function() {
+  var theImages = document.querySelectorAll('.image-holder'),
+  theHeading = document.querySelector('.heading'),
+  theSubhead = document.querySelector('.main-copy h2'),
+  theSeasonText = document.querySelector('.main-copy p'),
+  appliedClass;
 
+  function ChangeElements(){
+    //make sure event handling is working
+    debugger;
+  }
+
+  theImages.forEach(function(element, index){
+    //Loop through the images and add event handling to each one
+    element.addEventListener('click', ChangeElements, false);
+  })
+})();
